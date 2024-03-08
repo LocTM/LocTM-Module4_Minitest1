39b4bad5fce849c339841c1cfcb1c331d4bd353d
LocTM <tranminhloc.anhk@gmail.com>
2024-03-08 13:16:40 +0700

    add ITaskService to service

---
+++ src/main/java/com/codegym/service/ITaskService.java
@@ -0,0 +1,11 @@
+package com.codegym.service;
+
+import com.codegym.model.Task;
+import com.codegym.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface ITaskService extends IGenerateService<Task>{
+    Iterable<Task> findAllByProvince(Category category);
+}
